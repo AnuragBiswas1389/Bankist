7e259d4db8b9cc71f37a5736bc4c82a48a6c27b0
Anurag Biswas <88077771+AnuragBiswas1389@users.noreply.github.com>
2022-09-08 20:40:16 +0530

    Added the rest of the functions.

--- script.js
+++ script.js
@@ -128,7 +128,139 @@ const obsCallBack = function (entries, observer) {
 const obsOptions = {
   root: null,
   threshold: 0,
-  rootMargin:'-90px',
+  rootMargin: '-90px',
 };
 const observer = new IntersectionObserver(obsCallBack, obsOptions);
 observer.observe(header);
+
+//reveling sections using intersection observer api...
+/*
+1. create an observer api 
+2. attach to the elements
+3. attach the display altering class
+4. remove the class 
+5. remove the observer 
+*/
+
+const revelSections = new IntersectionObserver(revelSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+function revelSection(entries, observer) {
+  //destructuring the array into the elements
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) {
+    return;
+  }
+  entry.target.classList.remove('section--hidden');
+  //removing the elements
+  observer.unobserve(entry.target);
+}
+
+document.querySelectorAll('.section').forEach(function (ele) {
+  revelSections.observe(ele);
+  ele.classList.add('section--hidden');
+  console.log(ele);
+});
+
+//adding the lazy loading of images using intersection observer api and then changing the source of the target after the load event is invoked!
+
+const lazyLoadObserver = new IntersectionObserver(lazyLoad, {
+  root: null,
+  threshold: 0.15,
+  rootMargin: '-200px',
+});
+
+function lazyLoad(entry, observer) {
+  const [trgt] = entry;
+
+  if (!trgt.isIntersecting) {
+    return;
+  }
+  trgt.target.src = trgt.target.dataset.src;
+  trgt.target.addEventListener('load', function () {
+    trgt.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(trgt.target);
+}
+
+document
+  .querySelectorAll('img[data-src')
+  .forEach(ele => lazyLoadObserver.observe(ele));
+
+//adding the slider bar mechanism
+
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const slides = document.querySelectorAll('.slide');
+
+let currSlide = 0;
+navSlide(0);
+
+console.log(slides.length);
+function navSlide(no) {
+  slides.forEach(
+    (ele, i) => (ele.style.transform = `translateX(${100 * (i - no)}%`)
+  );
+}
+
+btnRight.addEventListener('click', rightSlide);
+btnLeft.addEventListener('click', leftSlide);
+
+function rightSlide() {
+  if (currSlide == slides.length - 1) {
+    currSlide = 0;
+    navSlide(0);
+  } else {
+    currSlide++;
+    navSlide(currSlide);
+  }
+  console.log(currSlide + ' ' + slides.length);
+}
+
+function leftSlide() {
+  if (currSlide == 0) {
+    currSlide = slides.length;
+  }
+  currSlide--;
+  navSlide(currSlide);
+}
+
+//second version for the slide bar
+const dots = document.querySelector('.dots');
+
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dots.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+    console.log(i);
+  });
+};
+createDots();
+
+dots.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const { slide } = e.target.dataset;
+    navSlide(slide);
+    activateDot(slide);
+  }
+});
+
+const activateDot = function (slide) {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+
+document.addEventListener('keydown', function (e) {
+  e.key == 'ArrowRight' && rightSlide();
+  e.key == 'ArrowLeft' && leftSlide();
+});
